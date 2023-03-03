74d4aa644e651139915383c39d0e13574c7179c9
Chris West (Faux) <git@goeswhere.com>
2023-03-03 13:04:13 +0000

    feat: skip empty images; cli speeds

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use std::time::Instant;
 use clap::Parser;
 use image::codecs::avif::AvifEncoder;
 use image::imageops::FilterType;
+use image::DynamicImage;
 use itertools::Itertools;
 use log::{debug, info};
+use rand::prelude::*;
 use rayon::prelude::*;
 use regex::Regex;
 
@@ -17,12 +19,35 @@ use regex::Regex;
 struct Cli {
     /// directory full of input images
     input: PathBuf,
+
+    /// avif speed; 0 (slowest) - 10 (fastest); meaning not clearly defined
+    ///
+    /// 4 means ~30 minutes; 9 is closer to ~10 minutes
+    #[clap(short, long, default_value = "6", value_parser = clap::value_parser!(u8).range(0..=10))]
+    speed: u8,
+
+    /// avif quality; 0 (terrible) - 100 (uselessly huge)
+    ///
+    /// 90 is very good, 70 is good, 60 is okay
+    #[clap(short, long, default_value = "70", value_parser = clap::value_parser!(u8).range(0..=100))]
+    quality: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct ImageOps {
+    quality: u8,
+    speed: u8,
 }
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
     let format = Regex::new(r".*_(-?\d+)_(-?\d+)\.")?;
     let args: Cli = Cli::parse();
+
+    let img_ops = ImageOps {
+        quality: args.quality,
+        speed: args.speed,
+    };
 
     let base_wh = 4096u32;
     let tile_wh = 256u32;
@@ -66,23 +91,33 @@ fn main() -> Result<()> {
         .map(|(x, y, path)| ((x, y), path))
         .collect::<HashMap<_, _>>();
 
-    let xys = (0..bw)
+    let mut xys = (0..bw)
         .flat_map(|x| (0..bh).map(move |y| (x, y)))
         .collect_vec();
+
+    // try not to process all the empty tiles at the same time
+    // (note that rayon already has a weird execution order)
+    xys.shuffle(&mut thread_rng());
 
     xys.into_par_iter().try_for_each(|(x, y)| -> Result<()> {
         let Some(base) = base_lookup.get(&(i64::from(x) + lx, i64::from(y) + ly)) else {
             return Ok(());
         };
 
-        let img = image::open(&base)?;
+        let img = image::open(base)?;
+        if is_entirely_transparent(&img) {
+            info!("skipping entirely transparent image {base:?}");
+            return Ok(());
+        }
         let mut time_manip = 0;
         let mut time_save = 0;
         for neg_zoom in 0..=4 {
             let mul = 2u32.pow(neg_zoom);
             let tiles = tile_per_base / mul;
             let step = tile_wh * mul;
-            let zoom = 7 - neg_zoom;
+            // 8 means 2^8 = 256 tiles; (4096 / 256px/tile) = 16 tiles per screenshot
+            // 256/16 = 16 screenshots; i.e. -8 -> 7
+            let zoom = 8 - neg_zoom;
             for ty in 0..tiles {
                 for tx in 0..tiles {
                     let dx = x * tiles + tx;
@@ -94,10 +129,14 @@ fn main() -> Result<()> {
 
                     let start = Instant::now();
                     let crop = img.crop_imm(tx * step, ty * step, step, step);
+                    if is_entirely_transparent(&crop) {
+                        debug!("skipping transparent cropped tile at {x}x{y} -> {tx}x{ty}");
+                        continue;
+                    }
                     let crop = crop.resize(tile_wh, tile_wh, FilterType::Lanczos3);
                     time_manip += start.elapsed().as_nanos();
                     let start = Instant::now();
-                    create_dir_and_save(dest, &crop)?;
+                    create_dir_and_save(dest, &crop, &img_ops)?;
                     time_save += start.elapsed().as_nanos();
                     debug!("saved {tx}x{ty} in {x}x{y} as {dx}x{dy}");
                 }
@@ -120,7 +159,18 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn create_dir_and_save(path: impl AsRef<Path>, img: &image::DynamicImage) -> Result<()> {
+/// only works for 8-bit images
+fn is_entirely_transparent(img: &DynamicImage) -> bool {
+    img.as_rgba8()
+        .map(|img| img.pixels().all(|p| p.0[3] == 0))
+        .unwrap_or(false)
+}
+
+fn create_dir_and_save(
+    path: impl AsRef<Path>,
+    img: &DynamicImage,
+    img_ops: &ImageOps,
+) -> Result<()> {
     let path = path.as_ref();
     fs::create_dir_all(
         path.parent()
@@ -128,9 +178,8 @@ fn create_dir_and_save(path: impl AsRef<Path>, img: &image::DynamicImage) -> Res
     )
     .with_context(|| anyhow!("creating directories for {path:?}"))?;
     let mut out = tempfile_fast::Sponge::new_for(path)?;
-    let enc = AvifEncoder::new_with_speed_quality(&mut out, 9, 60);
+    let enc = AvifEncoder::new_with_speed_quality(&mut out, img_ops.speed, img_ops.quality);
     enc.write_image(img.as_bytes(), img.width(), img.height(), img.color())?;
     out.commit()?;
-    // img.save_with_format(path, ImageFormat::Avif)?;
     Ok(())
 }
